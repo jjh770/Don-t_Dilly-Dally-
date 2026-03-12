297c29a286caf493d7f34b802aae753c81a45e9c
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-03-12 18:48:11 +0900

    feat: 플레이어 이동

---
+++ Assets/02.Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+}
---
+++ Assets/02.Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleRotation();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(h, 0, v).normalized;
+    }
+
+    private void HandleRotation()
+    {
+        if (moveDirection.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 velocity = moveDirection * _moveSpeed;
+        velocity.y = rb.linearVelocity.y;
+        rb.linearVelocity = velocity;
+    }
+}
