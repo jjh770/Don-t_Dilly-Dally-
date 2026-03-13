0b1af2d35a2938e18c034c6f7da542b79cb82806
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-03-13 13:51:38 +0900

    feat: 아이템 들고 놓기

---
+++ Assets/02.Scripts/Player/InteractableItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class InteractableItem : MonoBehaviour, IInteractable
+{
+    public bool IsHeld { get; private set; }
+
+    private Rigidbody _rigidbody;
+    private Collider _collider;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        IsHeld = true;
+        _rigidbody.isKinematic = true;
+        _collider.enabled = false;
+    }
+
+    public void Drop()
+    {
+        IsHeld = false;
+        _rigidbody.isKinematic = false;
+        _collider.enabled = true;
+    }
+}
---
+++ Assets/02.Scripts/Player/PlayerInteractionAbility.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class PlayerInteractionAbility : MonoBehaviour
+{
+    [SerializeField] private float _detectionRadius = 2f;
+    [SerializeField] private Transform _holdPoint;
+    [SerializeField] private LayerMask _interactableLayer;
+
+    private InteractableItem _currentItem;
+    private InteractableItem _nearestItem;
+
+    private void Update()
+    {
+        FindNearestInteractable();
+        HandleInteractInput();
+    }
+
+    private void FindNearestInteractable()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _detectionRadius, _interactableLayer);
+
+        _nearestItem = null;
+        float nearestDistance = float.MaxValue;
+
+        foreach (Collider col in colliders)
+        {
+            if (col.TryGetComponent(out InteractableItem item) && !item.IsHeld)
+            {
+                float distance = Vector3.Distance(transform.position, col.transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearestDistance = distance;
+                    _nearestItem = item;
+                }
+            }
+        }
+    }
+
+    private void HandleInteractInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_currentItem != null)
+            {
+                DropItem();
+            }
+            else if (_nearestItem != null)
+            {
+                PickUpItem(_nearestItem);
+            }
+        }
+    }
+
+    private void PickUpItem(InteractableItem item)
+    {
+        _currentItem = item;
+        _currentItem.Interact(gameObject);
+        _currentItem.transform.SetParent(_holdPoint);
+        _currentItem.transform.localPosition = Vector3.zero;
+        _currentItem.transform.localRotation = Quaternion.identity;
+    }
+
+    private void DropItem()
+    {
+        _currentItem.Drop();
+        _currentItem.transform.SetParent(null);
+        _currentItem = null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _detectionRadius);
+    }
+}
