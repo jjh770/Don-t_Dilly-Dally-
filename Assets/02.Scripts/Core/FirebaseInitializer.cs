9d27e90cf13c5a41a41064d6280fd378fc38fd13
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-03-24 14:13:04 +0900

    Feat : ParrelSync

--- Assets/02.Scripts/Core/FirebaseInitializer.cs
+++ Assets/02.Scripts/Core/FirebaseInitializer.cs
@@ -9,13 +9,15 @@ public class FirebaseInitializer : PersistentSingleton<FirebaseInitializer>
 {
     private Firebase.FirebaseApp _app = null;
     public FirebaseFirestore Database { get; private set; }
-    public bool IsInitialized => _app != null && Database != null;
 
     public static event Action OnFirebaseInitialized;
 
     protected override void Awake()
     {
         base.Awake();
+
+        if (Instance != this) return;
+
         FirebaseInit().Forget(e => UnityEngine.Debug.LogException(e));
     }
 
@@ -30,6 +32,14 @@ public class FirebaseInitializer : PersistentSingleton<FirebaseInitializer>
             _app = Firebase.FirebaseApp.DefaultInstance; // FirebaseApp 모듈 가져오기.
 
             Database = FirebaseFirestore.DefaultInstance; // Firestore 모듈 가져오기.
+
+            #if UNITY_EDITOR
+            if (ParrelSync.ClonesManager.IsClone())
+            {
+            // 클론 에디터에서만 오프라인 캐시를 꺼서 충돌(Lock)을 방지합니다.
+            Database.Settings.PersistenceEnabled = false;
+            }
+            #endif
 
             Debug.Log("[FirebaseInitializer] Firebase 초기화 성공");
             Debug.Log(FirebaseApp.DefaultInstance.Options.ProjectId);
