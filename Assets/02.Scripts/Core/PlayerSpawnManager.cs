4092c2e1f9db9c630b65650cf38b73acc76f9391
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-03-30 14:29:16 +0900

    Feat: 플레이어 랜덤 스폰 (중복X)

--- Assets/02.Scripts/Core/PlayerSpawnManager.cs
+++ Assets/02.Scripts/Core/PlayerSpawnManager.cs
@@ -1,7 +1,9 @@
 using Photon.Pun;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PhotonView))]
 public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
 {
     [SerializeField] private Transform[] _spawnPoints;
@@ -9,6 +11,8 @@ public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
 
     private GameObject _player;
     private bool _hasSpawnedLocalPlayer;
+
+    private Dictionary<int, int> _usedSpawnPoints = new Dictionary<int, int>();
 
     public event Action OnRespawn;
     public event Action<GameObject> OnPlayerSpawned;
@@ -28,20 +32,90 @@ public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
         if (!PhotonNetwork.InRoom) return;
         if (_hasSpawnedLocalPlayer) return;
 
-        Spawn();
-        _hasSpawnedLocalPlayer = _player != null;
+        _hasSpawnedLocalPlayer = true;
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            int spawnIndex = GetAvailableSpawnPointIndex();
+            if (spawnIndex < 0)
+            {
+                Debug.LogError("[PlayerSpawnManager] 사용 가능한 스폰 포인트가 없습니다.");
+                return;
+            }
+
+            int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+            _usedSpawnPoints[actorNumber] = spawnIndex;
+
+            photonView.RPC(nameof(RPC_SyncSpawnPoint), RpcTarget.Others, actorNumber, spawnIndex);
+            SpawnAt(spawnIndex);
+        }
+        else
+        {
+            photonView.RPC(nameof(RPC_RequestSpawnPoint), RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber);
+        }
     }
 
-    public void Spawn()
+    [PunRPC]
+    private void RPC_RequestSpawnPoint(int actorNumber)
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        int spawnIndex = GetAvailableSpawnPointIndex();
+        if (spawnIndex < 0)
+        {
+            Debug.LogError($"[PlayerSpawnManager] 플레이어 {actorNumber}에게 할당할 스폰 포인트가 없습니다.");
+            return;
+        }
+
+        _usedSpawnPoints[actorNumber] = spawnIndex;
+        photonView.RPC(nameof(RPC_AssignSpawnPoint), RpcTarget.All, actorNumber, spawnIndex);
+    }
+
+    [PunRPC]
+    private void RPC_SyncSpawnPoint(int actorNumber, int spawnIndex)
+    {
+        _usedSpawnPoints[actorNumber] = spawnIndex;
+    }
+
+    [PunRPC]
+    private void RPC_AssignSpawnPoint(int actorNumber, int spawnIndex)
+    {
+        _usedSpawnPoints[actorNumber] = spawnIndex;
+
+        if (actorNumber == PhotonNetwork.LocalPlayer.ActorNumber)
+        {
+            SpawnAt(spawnIndex);
+        }
+    }
+
+    private int GetAvailableSpawnPointIndex()
     {
         if (_spawnPoints == null || _spawnPoints.Length == 0)
         {
             Debug.LogError("[PlayerSpawnManager] 스폰 포인트가 설정되지 않았습니다.");
-            return;
+            return -1;
         }
 
-        int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.Length);
-        Vector3 spawnPosition = _spawnPoints[randomIndex].position;
+        List<int> availableIndices = new List<int>();
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            if (!_usedSpawnPoints.ContainsValue(i))
+            {
+                availableIndices.Add(i);
+            }
+        }
+
+        if (availableIndices.Count == 0)
+        {
+            return -1;
+        }
+
+        return availableIndices[UnityEngine.Random.Range(0, availableIndices.Count)];
+    }
+
+    private void SpawnAt(int spawnIndex)
+    {
+        Vector3 spawnPosition = _spawnPoints[spawnIndex].position;
         Quaternion backwardRotation = Quaternion.Euler(0, 180f, 0);
 
         _player = PhotonNetwork.Instantiate(_playerPrefab.name, spawnPosition, backwardRotation);
@@ -54,5 +128,10 @@ public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
 
         OnPlayerSpawned?.Invoke(_player);
         PlayerProperty.SetReadyState(false);
+    }
+
+    public void Spawn()
+    {
+        TrySpawnLocalPlayer();
     }
 }
