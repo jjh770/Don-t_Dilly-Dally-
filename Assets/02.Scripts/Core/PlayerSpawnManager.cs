8972fc797e081da695549ec5a88cd9e965495fb8
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-03-30 14:19:58 +0900

    Fix: 플레이어 상호작용 범위 변경

--- Assets/02.Scripts/Core/PlayerSpawnManager.cs
+++ Assets/02.Scripts/Core/PlayerSpawnManager.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
 {
     [SerializeField] private Transform[] _spawnPoints;
-
-    [SerializeField] private Collider _spawnArrange;
-
     [SerializeField] private GameObject _playerPrefab;
 
     private GameObject _player;
-
-    public event Action OnRespawn;
-
     private bool _hasSpawnedLocalPlayer;
 
+    public event Action OnRespawn;
     public event Action<GameObject> OnPlayerSpawned;
+
     public override void OnJoinedRoom()
     {
         TrySpawnLocalPlayer();
@@ -40,39 +36,23 @@ public class PlayerSpawnManager : PunSingleton<PlayerSpawnManager>
     {
         if (_spawnPoints == null || _spawnPoints.Length == 0)
         {
-            Debug.LogWarning("등록된 스폰 포인트가 없습니다.");
+            Debug.LogError("[PlayerSpawnManager] 스폰 포인트가 설정되지 않았습니다.");
             return;
         }
 
-        //int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.Length);
-        int Index = PhotonServerManager.Instance.CountOfPlayers;
-        Vector3 spawnPosition = GetRandomPointInBoxCollider();
-
-        // 리소스 폴더에서 "Player" 이름을 가진 프리팹을 생성하고, 서버에 등록함
-        // 리소스 폴더는 좋지 않음 => 다른 방법을 찾아보자
+        int randomIndex = UnityEngine.Random.Range(0, _spawnPoints.Length);
+        Vector3 spawnPosition = _spawnPoints[randomIndex].position;
         Quaternion backwardRotation = Quaternion.Euler(0, 180f, 0);
-        _player = PhotonNetwork.Instantiate(_playerPrefab.name, spawnPosition, backwardRotation);
 
-        
+        _player = PhotonNetwork.Instantiate(_playerPrefab.name, spawnPosition, backwardRotation);
 
         if (_player == null)
         {
-            Debug.LogWarning("플레이어 프리팹을 생성하는 데 실패했습니다. 'Player' 프리팹이 Resources 폴더에 있는지 확인하세요.");
+            Debug.LogError("[PlayerSpawnManager] 플레이어 프리팹 생성 실패. Resources 폴더에 프리팹이 있는지 확인하세요.");
             return;
         }
 
         OnPlayerSpawned?.Invoke(_player);
         PlayerProperty.SetReadyState(false);
-    }
-
-    public Vector3 GetRandomPointInBoxCollider()
-    {
-        Vector3 center = _spawnArrange.bounds.center;
-        Vector3 size = _spawnArrange.bounds.size;
-
-        float randomX = UnityEngine.Random.Range(center.x - size.x / 2f, center.x + size.x / 2f);
-        float randomZ = UnityEngine.Random.Range(center.z - size.z / 2f, center.z + size.z / 2f);
-
-        return new Vector3(randomX, 0f, randomZ);
     }
 }
