d84b68d8de7527a55ac514e19e46a4ff9ac3e239
정종혁 <whdgur0068@gmail.com>
2026-03-31 15:04:37 +0900

    Feat : TrashBox 풀반납 기능 추가

---
+++ Assets/02.Scripts/Recipe/Machines/TrashBoxInteractable.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace DontDillyDally.Data
+{
+    [RequireComponent(typeof(Collider))]
+    public class TrashBoxInteractable : MonoBehaviourPun, IInteractable, IItemAcceptor
+    {
+        private readonly HashSet<int> _processingItemIds = new HashSet<int>();
+
+        public bool IsInteracting => false;
+        public Transform Transform => transform;
+
+        public void Interact(Transform interactor)
+        {
+            if (interactor == null)
+            {
+                return;
+            }
+
+            IHeldItemInteractor heldItemInteractor = interactor.GetComponent<IHeldItemInteractor>();
+            if (heldItemInteractor == null)
+            {
+                return;
+            }
+
+            ItemObject heldItem = heldItemInteractor.CurrentHeldItem;
+            if (!CanAcceptItem(heldItem))
+            {
+                return;
+            }
+
+            if (!heldItemInteractor.TryReleaseHeldItem(heldItem))
+            {
+                return;
+            }
+
+            if (ItemRecycleUtility.TryRecycle(heldItem))
+            {
+                return;
+            }
+
+            if (heldItem.TryGetComponent(out IInteractable interactable))
+            {
+                heldItemInteractor.TryPickupInteractable(interactable);
+            }
+        }
+
+        public void StopInteract()
+        {
+        }
+
+        public bool CanAcceptItem(ItemObject item)
+        {
+            return item != null;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision == null)
+            {
+                return;
+            }
+
+            TryTrashFromCollider(collision.collider);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other == null)
+            {
+                return;
+            }
+
+            TryTrashFromCollider(other);
+        }
+
+        private void TryTrashFromCollider(Collider other)
+        {
+            if (other == null)
+            {
+                return;
+            }
+
+            ItemObject itemObject = other.GetComponentInParent<ItemObject>();
+            if (itemObject == null || !CanAcceptItem(itemObject))
+            {
+                return;
+            }
+
+            HoldableItem holdableItem = itemObject.GetComponent<HoldableItem>();
+            if (holdableItem != null && (holdableItem.IsInteracting || holdableItem.IsStoredInContainer))
+            {
+                return;
+            }
+
+            PhotonView itemPhotonView = itemObject.GetComponent<PhotonView>();
+            if (PhotonNetwork.InRoom && itemPhotonView != null && !itemPhotonView.IsMine && !itemPhotonView.AmController)
+            {
+                return;
+            }
+
+            int itemInstanceId = itemObject.GetInstanceID();
+            if (!_processingItemIds.Add(itemInstanceId))
+            {
+                return;
+            }
+
+            StartCoroutine(ReleaseProcessingLockNextFrame(itemInstanceId));
+            ItemRecycleUtility.TryRecycle(itemObject);
+        }
+
+        private IEnumerator ReleaseProcessingLockNextFrame(int itemInstanceId)
+        {
+            yield return null;
+            _processingItemIds.Remove(itemInstanceId);
+        }
+    }
+}
