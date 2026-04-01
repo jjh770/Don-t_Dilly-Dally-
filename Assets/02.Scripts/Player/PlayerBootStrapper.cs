87e1fe2bd5efe78d3a162609af2aa88e6482f85d
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-04-01 09:55:32 +0900

    Refactor : Player Presenter Model 제거

--- Assets/02.Scripts/Player/PlayerBootStrapper.cs
+++ Assets/02.Scripts/Player/PlayerBootStrapper.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class PlayerBootStrapper : MonoBehaviour
 {
-    PlayerModel _playerModel;
     PlayerView _playerView;
     PlayerPresenter _playerPresenter;
 
     private void Start()
     {
         Player owner = GetComponent<PhotonView>().Owner;
-        _playerModel = new PlayerModel(
-            PlayerProperty.GetNickname(owner),
-            PlayerProperty.GetReadyState(owner),
-            owner.IsMasterClient
-        );
         _playerView = GetComponentInChildren<PlayerView>();
 
-        _playerPresenter = new PlayerPresenter(_playerModel, _playerView, owner);
+        _playerPresenter = new PlayerPresenter(_playerView, owner);
         _playerView.Initialize(_playerPresenter);
         _playerPresenter.Initialize();
     }
