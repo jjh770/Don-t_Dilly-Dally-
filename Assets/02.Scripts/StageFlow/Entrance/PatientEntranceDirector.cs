daa101241d9fb786baa813735f9029522a4ff297
AE86T <mknoh1004@naver.com>
2026-04-04 10:57:10 +0900

    Feat : 환자 빛줄기 소환

--- Assets/02.Scripts/StageFlow/Entrance/PatientEntranceDirector.cs
+++ Assets/02.Scripts/StageFlow/Entrance/PatientEntranceDirector.cs
@@ -201,6 +201,10 @@ public class PatientEntranceDirector : MonoBehaviour
         {
             PlayEntranceByIndex(2);
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PlayEntranceByIndex(3);
+        }
 #endif
     }
 
