21eb15855763ba22bc60aa948d719f4ec57a62a8
g1-1g <111170307+g1-1g@users.noreply.github.com>
2026-04-07 19:07:54 +0900

    Refactor : 코드 리뷰 반영

--- Assets/02.Scripts/Core/SceneLoadManager.cs
+++ Assets/02.Scripts/Core/SceneLoadManager.cs
@@ -109,9 +109,7 @@ public class SceneLoadManager : PunPersistentSingleton<SceneLoadManager>
     {
         _loadingProgress = 0f;
 
-        string sceneName = _nextSceneData.SceneName;
-
-        sceneName = overrideSceneName ?? _nextSceneData.SceneName;
+        string sceneName = overrideSceneName ?? _nextSceneData.SceneName;
 
         ESceneLoadMode loadMode = _nextSceneData.SceneLoadMode;
 
