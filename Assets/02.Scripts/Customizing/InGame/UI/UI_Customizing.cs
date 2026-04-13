a633080b2590f6ca3d0023b10d304fde2cca3c37
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-04-13 13:28:17 +0900

    Revert "Fix/jhy customizing"

--- Assets/02.Scripts/Customizing/InGame/UI/UI_Customizing.cs
+++ Assets/02.Scripts/Customizing/InGame/UI/UI_Customizing.cs
@@ -66,7 +66,6 @@ public class UI_Customizing : UIPopupBase
 
     private void OnDestroy()
     {
-        ClearItemButtons();
         UnsubscribeFromViewModel();
         _viewModel?.Dispose();
     }
@@ -180,10 +179,7 @@ public class UI_Customizing : UIPopupBase
         foreach (var viewData in _viewModel.VisibleItems)
         {
             var button = CreateItemButton(viewData);
-            if (button != null)
-            {
-                _itemButtons.Add(button);
-            }
+            _itemButtons.Add(button);
         }
     }
 
@@ -220,10 +216,7 @@ public class UI_Customizing : UIPopupBase
 
         for (int i = 0; i < _itemButtons.Count && i < visibleItems.Count; i++)
         {
-            if (_itemButtons[i] != null)
-            {
-                _itemButtons[i].SetSelected(visibleItems[i].IsSelected);
-            }
+            _itemButtons[i].SetSelected(visibleItems[i].IsSelected);
         }
     }
 
