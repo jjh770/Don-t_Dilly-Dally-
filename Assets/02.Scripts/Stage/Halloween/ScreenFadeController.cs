2c17afa584184dda3d2835f9ac792af44934db8f
정종혁 <whdgur0068@gmail.com>
2026-04-20 17:23:05 +0900

    Feat : 할로윈 기믹 사운드 추가, StageFlowManager 불필요한 프리팹 제거

--- Assets/02.Scripts/Stage/Halloween/ScreenFadeController.cs
+++ Assets/02.Scripts/Stage/Halloween/ScreenFadeController.cs
@@ -20,6 +20,8 @@ public class ScreenFadeController : MonoBehaviour
     private PhotonView _photonView;
     private Coroutine _blackoutCoroutine;
 
+    private AudioSource _horrorTheme;
+
     private void Start()
     {
         _photonView = GetComponent<PhotonView>();
@@ -57,7 +59,9 @@ public class ScreenFadeController : MonoBehaviour
         {
             StopCoroutine(_blackoutCoroutine);
         }
-        //SoundManager.Instance.Play(SFXKey.StartBlackOut, SoundType.Local);
+        SoundManager.Instance.Play(SFXKey.StartBlackOut, SoundType.Local);
+        _horrorTheme = SoundManager.Instance.PlayLoop(SFXKey.HorrorTheme);
+        SoundManager.Instance.DuckBGM(0.1f, 0.3f);
         _blackoutCoroutine = StartCoroutine(FadeToBlack());
     }
 
@@ -68,7 +72,9 @@ public class ScreenFadeController : MonoBehaviour
         {
             StopCoroutine(_blackoutCoroutine);
         }
-        //SoundManager.Instance.Play(SFXKey.EndBlackOut, SoundType.Local);
+        SoundManager.Instance.Play(SFXKey.EndBlackOut, SoundType.Local);
+        SoundManager.Instance.StopSFX(_horrorTheme, true);
+        SoundManager.Instance.DuckBGM(1f, 0.4f);
         _blackoutCoroutine = StartCoroutine(FadeToNormal());
     }
 
